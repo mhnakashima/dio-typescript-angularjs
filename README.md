377cecb3a922ca5be6924f752c8cb7ad482866aa
michael haruo nakashima <mhnakashima@gmail.com>
2021-07-25 22:08:31 -0300

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # dio-typescript-angularjs
-Conversao de um projeto em AngularJs para Typescript
+Creating a Project using AngularJS and Typescript
+
+- To Run - download or clone the repository
+- install packages - npm i
+- npm run dev
+
+
+# ######## Exercises #########
+- Add no results
+- Add a component
+- Create a Coin Class
+- Add a new module
--- src/modules/dashboard/detail/DetailController.ts
+++ src/modules/dashboard/detail/DetailController.ts
@@ -1,28 +1,6 @@
-/* (function () {
-  "use strict";
-
-  app.controller("DetailController", DetailController);
-
-  function DetailController($scope, apiService, $stateParams) {
-    var vm = $scope;
-    vm.coin = {};
-
-    vm.init = function () {
-      const coins = apiService.getCoinStorage() || [];
-      if($stateParams?.name){
-        vm.coin = coins.find(coin => coin.name === $stateParams.name);
-        console.log(vm.coin);
-      }
-    };
-    vm.init();
-  }
-})();*/
-
-import { IOnInit } from "angular";
 import { IStateParamsService } from "angular-ui-router";
 import { ApiRequestService } from "../../services/ApiRequest.service";
-
-export class DetailController{
+export class DetailController {
 
   public coin = {};
 
@@ -32,9 +10,8 @@ export class DetailController{
 
   private loadData(): void {
     const coins = this.ApiRequestService.getCoinStorage() || [];
-    if(this.$stateParams?.name){
+    if (this.$stateParams?.name) {
       this.coin = coins.find(coin => coin.name === this.$stateParams.name);
-      console.log(this.coin);
     }
   }
 }
